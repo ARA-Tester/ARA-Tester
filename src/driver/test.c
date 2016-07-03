229e58e0af99be696fca2f2522226ab7d967b1d1
Ivo Stratev <ivo.stratev.tues@gmail.com>
2016-07-03 16:43:48 +0300

    Update test.c

--- src/driver/test.c
+++ src/driver/test.c
@@ -4,20 +4,25 @@
 #include <errno.h>
 #include <stdio.h>
 #include <sys/ioctl.h>
-
+#include <ara_tester_ioctl.h>
 
 int main(void) {
     int fd0 = open("/dev/ara_tester_axis0", 3);
+    unsigned long get;
     printf("error %d\nfd %d\n", errno, fd0);
+    errno = 0;
+    ioctl(fd0, ARA_TESTER_SET, 1000000000);
+    printf("ioctl %d ARA_TESTER_SET 0\n", errno);
+    errno = 0;
+    ioctl(fd0, ARA_TESTER_GET, 1000000000);
+    printf("ioctl %d ARA_TESTER_GET 0 %lu\n", errno, get);
+    printf("error %d\nfd %d\n", errno, fd0);
+    errno = 0;
+    ioctl(fd0, ARA_TESTER_SET, 1000000000);
+    printf("ioctl %d ARA_TESTER_SET 0\n", errno);
+    errno = 0;
+    ioctl(fd0, ARA_TESTER_GET, 1000000000);
+    printf("ioctl %d ARA_TESTER_GET 0 %lu\n", errno, get);
     close(fd0);
-    int fd1 = open("/dev/ara_tester_axis1", 3);
-    printf("error %d\nfd %d\n", errno, fd1);
-    close(fd1);
-    int fd2 = open("/dev/ara_tester_axis2", 3);
-    printf("error %d\nfd %d\n", errno, fd2);
-    close(fd2);
-    int fd3 = open("/dev/ara_tester_axis3", 3);
-    printf("error %d\nfd %d\n", errno, fd3);
-    close(fd3);
     return 0;
 }
