e17d50e993e4a2761ac8a2fca9323494e60f8db2
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-09 17:17:44 +0000

    finally achived success with starting new movment only after last one has fineshed

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,41 +7,40 @@ const nanoSecToMiliSec = 1000000;
 
 let dir: boolean = true;
 let fd: number = openIoctlSync('ara_tester_axis0');
-let max: number = 5000000;
-let min: number = 5000;
+let max: number = 1000000;
+let min: number = 40000;
 let delta: number = 50;
-let linear: number = 1000;
+let linear: number = 90000;
+let count: number = 0;
 
 function movment(dir: boolean, max: number, min: number, delta: number, linear: number): number {
-    try {
-        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_DIR, Number(dir));
-        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, max);
-        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, min);
-        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
-        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, linear);
-        let exec: Ioctl = ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC);
-        let timeout: number = (max + min) / (((max - min) / delta) + 1);
-        return Math.floor((timeout + (exec.data * 10) + (min * linear)) / nanoSecToMiliSec) + 1000;
-    } catch(err) {
-        console.log(err);
-        return 0;
-    }
+    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_DIR, Number(dir));
+    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, max);
+    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, min);
+    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
+    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, linear);
+    let exec: Ioctl = ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC);
+    max /= nanoSecToMiliSec;
+    min /= nanoSecToMiliSec;
+    delta /= nanoSecToMiliSec;
+    let timeout: number = (max + min) * (((max - min) / delta) + 1);
+    return Math.floor(timeout + (exec.data * 10 / nanoSecToMiliSec) + (min * linear)) + 2000;
 }
 
 function timeout(callback: () => void): void {
-    var t: number = movment(dir, max, min, delta, linear);
-    dir = !dir;
-    console.log(t);
-    if(t) {
-        setTimeout(callback, t);
+    if(count === 6) {
+        closeSync(fd); 
     } else {
-        console.error('Something didn\'t worked');
-        closeSync(fd);
+        var t: number = movment(dir, max, min, delta, linear);
+        dir = !dir;
+        count++;
+        setTimeout(callback, t);
     }
 }
 
-timeout(() => {
-    timeout(() => {
-        closeSync(fd);
-    });
-});
+
+function movLoop(): void {
+	timeout(movLoop);
+}
+
+movLoop();
