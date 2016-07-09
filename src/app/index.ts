95b6feac4596fa943d7f7434f87c0f604fb64c2c
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-09 17:49:47 +0300

    testing timeout calculation algo

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,21 +3,48 @@ import { openIoctlSync } from 'open-ioctl';
 import { closeSync } from 'fs';
 import { ARA_TESTER } from './ARA_TESTER';
 
+let dir: boolean = true;
 let fd: number = openIoctlSync('ara_tester_axis0');
+let max: number = 1000000;
+let min: number = 40000;
+let delta: number = 50;
+let linear: number = 90000;
 
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_SET_DIR, 1));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, 1000000));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, 40000));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, 50));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, 90000));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_PAUSE));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_TOTAL));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_COUNTER));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_MOVMENT_STATE));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_ACTIVE));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_RESUME));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_ACTIVE));
-console.log(ioctl(fd, ARA_TESTER.ARA_TESTER_GET_COUNTER));
 
-closeSync(fd);
+function movment(dir: boolean, max: number, min: number, delta: number, linear: number): number {
+    try {
+        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_DIR, Number(dir);
+        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, max);
+        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, min);
+        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
+        ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_LINEAR, linear);
+        let exec: Ioctl = ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC);
+        let timeout: number = 0;
+        for(let i: number = max; i >= min; i -= delta) {
+            timeout += i / 1000;
+        }
+        return (timeout * 2) + Math.floor(exec.data / 100) + (min / 1000 * linear) + 1;
+
+    } catch(err: NodeJS.ErrnoException) {
+        console.log(err);
+        return 0;
+    }
+}
+
+function timeout(callback: () => void): void {
+    var t: number = movment(dir, max, min, delta, linear);
+    dir = !dir;
+    console.log(t);
+    if(t) {
+        setTimeout(callback, t);
+    } else {
+        console.error('Something didn\'t worked');
+        closeSync(fd);
+    }
+}
+
+timeout(() => {
+    timeout(() => {
+        closeSync(fd);
+    });
+});
