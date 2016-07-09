d88d8d6731f218a43c87fa3acff90b18a09708ea
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-09 19:06:26 +0300

    removing for loop sum of an arithmetic progression can be used instead

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,11 +7,10 @@ const nanoSecToMiliSec = 1000000;
 
 let dir: boolean = true;
 let fd: number = openIoctlSync('ara_tester_axis0');
-let max: number = 50000;
-let min: number = 4000;
+let max: number = 5000000;
+let min: number = 5000;
 let delta: number = 50;
-let linear: number = 1000000;
-
+let linear: number = 1000;
 
 function movment(dir: boolean, max: number, min: number, delta: number, linear: number): number {
     try {
@@ -21,12 +20,8 @@ function movment(dir: boolean, max: number, min: number, delta: number, linear: 
         ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
         ioctl(fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, linear);
         let exec: Ioctl = ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC);
-        let timeout: number = 0;
-        for(let i: number = max; i >= min; i -= delta) {
-            timeout += i / nanoSecToMiliSec;
-        }
-        return Math.floor((timeout * 2) + (exec.data / nanoSecToMiliSec * 10) + (min / nanoSecToMiliSec * linear)) + 1000;
-
+        let timeout: number = (max + min) / (((max - min) / delta) + 1);
+        return Math.floor((timeout + (exec.data * 10) + (min * linear)) / nanoSecToMiliSec) + 1000;
     } catch(err) {
         console.log(err);
         return 0;
