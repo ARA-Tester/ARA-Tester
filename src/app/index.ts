5ba770907922d911726c1d6acc24c9755e2b5f14
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-10 08:43:42 +0300

    wraping in a class and adding pause and resume functionality

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,42 +5,110 @@ import { ARA_TESTER } from './ARA_TESTER';
 
 const nanoSecToMiliSec = 1000000;
 
-let dir: boolean = true;
-let fd: number = openIoctlSync('ara_tester_axis0');
-let max: number = 1000000;
-let min: number = 40000;
-let delta: number = 50;
-let linear: number = 90000;
-let count: number = 0;
+class AraTesterAxis {
+    private _id: number;
+    private _fd: number;
+    private _total: number;
+    private _timeout: number;
+    private _lastMillis: number;
+    private _active: boolean;
+    private _pause: boolean;
+    private _timer: NodeJS.Timer;
+    private _resolve: (value: number) => void;
+    private _reject: (reason: NodeJS.ErrnoException) => void;
 
-function movment(dir: boolean, max: number, min: number, delta: number, linear: number): number {
-    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_DIR, Number(dir));
-    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, max);
-    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, min);
-    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
-    ioctl(fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, linear);
-    let exec: Ioctl = ioctl(fd, ARA_TESTER.ARA_TESTER_EXEC);
-    max /= nanoSecToMiliSec;
-    min /= nanoSecToMiliSec;
-    delta /= nanoSecToMiliSec;
-    let timeout: number = (max + min) * (((max - min) / delta) + 1);
-    return Math.floor(timeout + (exec.data * 10 / nanoSecToMiliSec) + (min * linear)) + 2000;
-}
+    private static millis(): number {
+        return (new Date()).getTime();
+    }
 
-function timeout(callback: () => void): void {
-    if(count === 6) {
-        closeSync(fd); 
-    } else {
-        var t: number = movment(dir, max, min, delta, linear);
-        dir = !dir;
-        count++;
-        setTimeout(callback, t);
+    private tryToResolve(): void {
+        let counter: Ioctl
+        try {
+            counter = ioctl(this._fd, ARA_TESTER.ARA_TESTER_GET_COUNTER);
+        } catch(err) {
+            return this._reject(err);
+        }
+        this._resolve(counter.data);
+    }
+
+    private setTimer(): void {
+        this._lastMillis = AraTesterAxis.millis();
+        this._pause = false;
+        this._timer = setTimeout(this.tryToResolve, this._timeout);
+    }
+
+    public constructor(id: number) {
+        this._id = id;
+        this._fd = openIoctlSync('ara_tester_axis' + id);
+        this._active = false;
+        this._pause = false;
+    }
+
+    public exec(dir: boolean, max: number, min: number, delta: number, linear: number): Promise<number> {
+        return new Promise<number>((resolve: (value: number) => void, reject: (reason: NodeJS.ErrnoException) => void) => {
+            let exec: Ioctl;
+            let timeout: number;
+            this._resolve = resolve;
+            this._reject = reject;
+            try {
+                ioctl(this._fd, ARA_TESTER.ARA_TESTER_SET_DIR, Number(dir));
+                ioctl(this._fd, ARA_TESTER.ARA_TESTER_SET_T_MAX, max);
+                ioctl(this._fd, ARA_TESTER.ARA_TESTER_SET_T_MIN, min);
+                ioctl(this._fd, ARA_TESTER.ARA_TESTER_SET_T_DELTA, delta);
+                ioctl(this._fd, ARA_TESTER.ARA_TESTER_SET_LINEAR, linear);
+                exec = ioctl(this._fd, ARA_TESTER.ARA_TESTER_EXEC);
+            } catch(err) {
+                return this._reject(err);
+            }
+            max /= nanoSecToMiliSec;
+            min /= nanoSecToMiliSec;
+            delta /= nanoSecToMiliSec;
+            timeout = (max + min) * (((max - min) / delta) + 1);
+            this._total = exec.data;
+            console.log("Total: " + this._total);
+            this._timeout = Math.floor(timeout + (this._total * 10 / nanoSecToMiliSec) + (min * linear)) + 2000;
+            this._active = true;
+            this.setTimer();
+        });
+    }
+
+    public pause(): void {
+        if(!this._pause && this._active) {
+            this._pause = true;
+            clearTimeout(this._timer);
+            this._timeout -= (AraTesterAxis.millis() - this._lastMillis);
+            ioctl(this._fd, ARA_TESTER.ARA_TESTER_PAUSE);
+        }
+    }
+
+    public resume(): void {
+        if(this._pause && this._active) {
+            ioctl(this._fd, ARA_TESTER.ARA_TESTER_RESUME);
+            this.setTimer();        
+        }
+    }
+
+    public release(): void {
+        closeSync(this._fd);
     }
 }
 
 
-function movLoop(): void {
-	timeout(movLoop);
-}
+let axis: AraTesterAxis = new AraTesterAxis(0);
+let dir: boolean = true;
+let max: number = 1000000;
+let min: number = 40000;
+let delta: number = 50;
+let linear: number = 90000;
+let exec: Promise<number> = axis.exec(dir, max, min, delta, linear);
+exec.then((count: number) => {
+    console.log("Counter: " + count);
+    axis.release();
+}).catch((err: NodeJS.ErrnoException) => console.log("Error: " + JSON.stringify(err)));
 
-movLoop();
+setTimeout(() => {
+    axis.pause();
+    setTimeout(() => {
+        axis.resume();
+    }, 2000)
+}, 9000);
