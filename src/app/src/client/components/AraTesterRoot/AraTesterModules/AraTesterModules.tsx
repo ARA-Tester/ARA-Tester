5ad3ff81ed7f3d4f942bbd93da24e485087395d5
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-19 14:59:35 +0300

    touch* evenst seems not firing so will try with mouse*

--- src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
@@ -4,11 +4,15 @@ import MovmentButton from './../../MovmentButton';
 export default class AraTesterModules extends React.Component<void, void> {
     public onTouchStart: React.TouchEventHandler;
     public onTouchEnd: React.TouchEventHandler;
+    public onMouseDown: React.MouseEventHandler;
+    public onMouseUp: React.MouseEventHandler;
 
     public constructor() {
         super();
         this.onTouchStart = this.handleTouchStart.bind(this);
         this.onTouchEnd = this.handleTouchEnd.bind(this);
+        this.onMouseDown = this.handleMouseDown.bind(this);
+        this.onMouseUp = this.handleMouseUp.bind(this);
     }
 
     public handleTouchStart(event: React.TouchEvent): void {
@@ -21,7 +25,24 @@ export default class AraTesterModules extends React.Component<void, void> {
         console.log(new Date());
     }
 
+    public handleMouseDown(event: React.MouseEvent): void {
+        console.log('down');
+        console.log(new Date());
+    }
+
+    public handleMouseUp(event: React.MouseEvent): void {
+        console.log('up');
+        console.log(new Date());
+    }
+
     public render(): JSX.Element {
-        return <MovmentButton movment="rotate left" onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd} />
+        return (
+            <MovmentButton
+                movment="rotate left"
+                onTouchStart={this.onTouchStart}
+                onTouchEnd={this.onTouchEnd}
+                onMouseDown={this.onMouseDown} 
+                onMouseUp={this.onMouseUp} />
+        );
     }
 }
--- src/app/src/client/components/BasicButtonProps.ts
+++ src/app/src/client/components/BasicButtonProps.ts
@@ -7,6 +7,8 @@ interface BasicButtonProps extends DisabledProp, OptionalStyleProp {
     onTouchTap?: TouchTapEventHandler;
     onTouchStart?: React.TouchEventHandler;
     onTouchEnd?: React.TouchEventHandler;
+    onMouseDown?: React.MouseEventHandler;
+    onMouseUp?: React.MouseEventHandler;
 }
 
 export default BasicButtonProps;
