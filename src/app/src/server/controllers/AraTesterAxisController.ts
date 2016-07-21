ab706516d7816558fe80212ed95164c812cd8a14
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-21 12:12:09 +0300

    clear code

--- src/app/src/server/controllers/AraTesterAxisController.ts
+++ src/app/src/server/controllers/AraTesterAxisController.ts
@@ -49,17 +49,17 @@ export default class AraTesterAxisController {
         return value - whole;
     }
 
-    private _calculateEven(comapre: number): void {
-        this._even = this._even >= comapre ? 1 : 0;
-    }
-
     private _proceedEven(progressive: number): number {
-        if(this._even >= 1.45) {
-            this._calculateEven(2.45);
+        if(this._even < 0.9) {
+            this._even = this._even > 0.4 ? 1 : 0;
+            return progressive;
+        }
+        if(this._even <= 1.4) {
+            this._even = 0;
             return progressive + 1;
         }
-        this._calculateEven(0.45);
-        return progressive;
+        this._even = 1;
+        return progressive + 1;
     }
 
     private _exec(): void {
