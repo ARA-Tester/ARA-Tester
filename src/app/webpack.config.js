ee611b0fcf5e3fabe2b142417767d6e4e0f84f6d
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-21 14:22:49 +0300

    adding css loading support

--- src/app/webpack.config.js
+++ src/app/webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
 
     module: {
         loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
             { test: /\.tsx?$/, loader: "ts-loader" }
         ],
     }
