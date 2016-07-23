226514e7e750c0d720e8408f00e938790ee302a5
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-23 15:12:57 +0300

    still no center testing with this

--- src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
@@ -15,7 +15,7 @@ export default class AraTesterModules extends React.Component<OptionalStyleProp,
     public render(): JSX.Element {
         return (
             <div style={Object.assign({}, fillSpaceStyle, this.props.style)}>
-                <Flex column justifyContent="space-around" alignContent="space-around" alignItems="center">
+                <Flex column justifyContent="space-around" alignContent="space-around" alignItems="stretch" wrap={false}>
                     <Item flex>
                         <AraTesterControlls />
                     </Item>
