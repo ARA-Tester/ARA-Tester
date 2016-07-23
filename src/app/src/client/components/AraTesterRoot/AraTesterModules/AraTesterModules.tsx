be4a95f9a3df505f9373c61f360763af1acf668a
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-23 16:04:59 +0300

    vertical centering won't happen so some spacing should do the work

--- src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
@@ -6,25 +6,20 @@ import AraTesterControlls from './AraTesterControlls';
 import AraTesterPositions from './AraTesterPositions';
 const { div } = React.DOM;
 
-const fillSpaceStyle: React.CSSProperties = {
-    width: '100%',
-    height: '100%'
-};
+const topSpaceStyle: React.CSSProperties = {
+    marginTop: 25
+}
 
 export default class AraTesterModules extends React.Component<OptionalStyleProp, void> {
     public render(): JSX.Element {
         return (
-            <div style={Object.assign({}, fillSpaceStyle, this.props.style)}>
-                <Flex row justifyContent="center" alignContent="center" alignItems="center">
-                    <Item flex flexBasis="auto">
-                        <Flex column justifyContent="space-around" alignContent="space-around" alignItems="center">
-                            <Item flex flexBasis="auto">
-                                <AraTesterControlls />
-                            </Item>
-                            <Item flex flexBasis="auto">
-                                <AraTesterPositions />
-                            </Item>
-                        </Flex>
+            <div style={Object.assign({}, topSpaceStyle, this.props.style)}>
+                <Flex column justifyContent="center" alignContent="center" alignItems="center">
+                    <Item flex>
+                        <AraTesterControlls />
+                    </Item>
+                    <Item flex>
+                        <AraTesterPositions style={topSpaceStyle} />
                     </Item>
                 </Flex>
             </div>
