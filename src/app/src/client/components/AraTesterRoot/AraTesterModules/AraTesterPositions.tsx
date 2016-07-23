38d6280e166df7b10dddaadd51b1feb86ad14c22
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-23 13:37:43 +0300

    fixing broken tag

--- src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterPositions.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterPositions.tsx
@@ -24,7 +24,7 @@ export default class AraTesterModules extends React.Component<OptionalStyleProp,
                 <ListItem>
                     <AraTesterAxisCurrentPosition axisId={0} axisName="X" />
                 </ListItem>
-            </List
+            </List>
         );
     }
 }
