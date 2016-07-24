1eedbcccfb3b81da203e523c5e0922f954c9e1b7
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-24 10:52:09 +0300

    Tring to center using plain old CSS

--- src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterModules/AraTesterModules.tsx
@@ -8,17 +8,23 @@ const spacingStyle: React.CSSProperties = {
     margin: 30,
 };
 
-const childSpaceStyle: React.CSSProperties = Object.assign({
-    width: '100%',
-    height: '100%'
-}, spacingStyle);
+const centerStyle: React.CSSProperties = {
+    display: 'block',
+    margin: 'auto'
+};
+
+const wrapperStyle: React.CSSProperties = {
+    width: '60%'
+};
 
 export default class AraTesterModules extends React.Component<OptionalStyleProp, void> {
     public render(): JSX.Element {
         return (
-            <div style={Object.assign({}, childSpaceStyle, this.props.style)}>
-                <AraTesterPositions style={spacingStyle} />
-                <AraTesterControlls style={spacingStyle} />
+            <div style={Object.assign({}, centerStyle, this.props.style)}>
+                <div style={wrapperStyle}>
+                    <AraTesterPositions style={spacingStyle} />
+                    <AraTesterControlls style={spacingStyle} />
+                </div>
             </div>
         );
     }
