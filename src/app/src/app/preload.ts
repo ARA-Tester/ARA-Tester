c8cb49b3e8c01c6064adf7142cb6a95475628f47
NoHomey <ivo.stratev.tues@gmail.com>
2016-07-27 08:53:48 +0300

    testing if app will load from desktop

--- src/app/src/app/preload.ts
+++ src/app/src/app/preload.ts
@@ -3,6 +3,8 @@ import { Config } from './../share/config';
 
 const host: string = `${Ip.address()}:${Config.PORT}`;
 
+alert(host);
+
 process.once('loaded', () => {
   window.location.host = host;
 });
