889b5e396f97bc612bcdd8bfe21b1d219a72002e
NoHomey <ivo.stratev.tues@gmail.com>
2016-08-27 21:46:46 +0300

    since modules development is done separatly it will be commented until it's done and merged correctly not just commited

--- src/app/src/client/components/AraTesterRoot/AraTesterRoot.tsx
+++ src/app/src/client/components/AraTesterRoot/AraTesterRoot.tsx
@@ -3,7 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import ActionViewQuilt from 'material-ui/svg-icons/action/view-quilt';
-import AraTesterModules from './AraTesterModules/AraTesterModules';
+//import AraTesterModules from './AraTesterModules/AraTesterModules';
 import AraTesterConfigWrapper from './AraTesterConfigWrapper/AraTesterConfigWrapper';
 const { div, link } = React.DOM;
 
@@ -19,7 +19,7 @@ export default class AraTesterRoot extends React.Component<void, void> {
                     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css"/>
                     <Tabs>
                         <Tab icon={<ActionViewQuilt />} label="Modules">
-                            <AraTesterModules />
+                            {/* <AraTesterModules /> */}
                         </Tab>
                         <Tab icon={<ActionSettings />} label="Configurate">
                             <AraTesterConfigWrapper axisId={0} positive="right" negative="left" />
